e56e90e528ee42bfaa542da915e2603f0a5fd033
jlessoff <jlessoff@gmail.com>
2021-10-28 14:24:01 +0200

    Commit Oct 28th.  reading in data from external sources

--- Restaurants.py
+++ Restaurants.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sodapy import Socrata
-import datetime
 import matplotlib.pyplot as plt
 from secret import app_token,username,password
-import seaborn as sns
 
 
 client = Socrata("data.cityofnewyork.us",
@@ -21,35 +19,9 @@ results_df=results_df.drop(['phone', 'community_board', 'council_district', 'bin
 results_df['inspection_date'] = pd.to_datetime(results_df['inspection_date'])
 trim = results_df.loc[results_df['inspection_date'].dt.year.isin([2017, 2018, 2019, 2020, 2021])]
 #
-# plt.style.use('ggplot')
-# sns.set(style='darkgrid')
-# green = (0.3333333333333333, 0.6588235294117647, 0.40784313725490196)
-# orange = (0.8666666666666667, 0.5176470588235295, 0.3215686274509804)
-
 
 # plt.savefig('img/borosgrades.png')
 count=results_df['grade'].value_counts().plot(kind='bar', color=['green','red','blue','yellow'])
 plt.xlabel("Grade", labelpad=14)
 plt.ylabel("Count of Grade", labelpad=14)
 plt.show()
-
-#results_df = results_df.loc[results_df['inspection_date'].dt.year.isin([2017, 2018, 2019, 2020,2021])]
-
-
-#find highest number of critical flags per exam, grouping by inspection date and restaurant name
-#results1=results_df.groupby(['critical_flag','dba','grade']).count().sort_values(by='grade', ascending=False)
-#print(results_df)
-
-#count of exams per restaurant
-#results1=results_df.groupby(['dba']).count().sort_values(by='dba', ascending=False)
-
-
-#
-#
-# results_df_critical=results_df[results_df['critical_flag'].isin(['Critical','Not Critical'])]
-#
-#
-# print(results_df_critical)
-#
-# results=results_df['critical_flag']
-# print(results)
--- Yelp.py
+++ Yelp.py
@@ -1,16 +1,42 @@
-# Define function to gather keys:
-def get_keys(path):
-    with open(path) as f:
-        return json.load(f)
+import requests
+import json
 
 
-# Pull in keys and specifically draw out the api key. I have removed the specific path to the keys
-# for security purposes:
-keys = get_keys("/Users/'INSERTPATH'/yelp_api.json")
-api_key = keys['J65Cnx9_2hwl_VndK1zLq4vK5b4GNwjwfmMz9RagM5gI4hUE_ZUifGEo9X1xj1LbqIMPSKbfj7rrK295GCAhjg6cwJItTpiK97oaMuMwSD0mgrT-_eyFw9m3d0tlYXYx']
+api_key='J65Cnx9_2hwl_VndK1zLq4vK5b4GNwjwfmMz9RagM5gI4hUE_ZUifGEo9X1xj1LbqIMPSKbfj7rrK295GCAhjg6cwJItTpiK97oaMuMwSD0mgrT-_eyFw9m3d0tlYXYx'
+headers = {'Authorization': 'Bearer %s' % api_key}
 
-# URL to pull data from:
+url='https://api.yelp.com/v3/businesses/search'
+
+params = {'term':'seafood','location':'New York City'}
+req = requests.get(url, headers=headers)
+parsed = json.loads(req.text)
+
+
+#
+
 url = 'https://api.yelp.com/v3/businesses/search'
 
-# Identify headers:
-headers = {'Authorization': 'Bearer {}'.format(api_key)}
+params = {'location':'New York City'}
+req = requests.get(url, params=params, headers=headers)
+
+parsed = json.loads(req.text)
+businesses = parsed["businesses"]
+for business in businesses:
+    print("Name:", business["name"])
+    print("Rating:", business["rating"])
+    print("Address:", " ".join(business["location"]["display_address"]))
+    print("Phone:", business["phone"])
+    print("\n")
+    id = business["id"]
+    url = "https://api.yelp.com/v3/businesses/" + id + "/reviews"
+    req = requests.get(url, headers=headers)
+    parsed = json.loads(req.text)
+    reviews = parsed["reviews"]
+    for review in reviews:
+        print("User:", review["user"]["name"], "Rating:", review["rating"], "Review:", review["text"], "\n")
+
+
+
+
+
+
