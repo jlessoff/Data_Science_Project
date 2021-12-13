3251dd72701a45b8d470e7ababfb12e64f2e033e
jlessoff <jlessoff@gmail.com>
2021-12-13 11:45:51 +0100

    Not grouped by year

--- Rodents.py
+++ Rodents.py
@@ -12,7 +12,7 @@ client = Socrata("data.cityofnewyork.us",
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("p937-wjvj",select='date_trunc_y(inspection_date),zip_code,result,count(result),latitude,longitude',order= 'date_trunc_y(inspection_date)',group="date_trunc_y(inspection_date),zip_code,result,latitude,longitude",where='result="Rat Activity" and latitude>0 and( date_trunc_y(inspection_date)="2018" or date_trunc_y(inspection_date)="2019" or date_trunc_y(inspection_date)="2020")', limit=2000000)
+results = client.get("p937-wjvj",select='date_trunc_y(inspection_date),zip_code,result,latitude,longitude',order= 'date_trunc_y(inspection_date)',where='result="Rat Activity" and latitude>0 and( date_trunc_y(inspection_date)="2018" or date_trunc_y(inspection_date)="2019" or date_trunc_y(inspection_date)="2020")', limit=2000000)
 #print(results)
 # # Convert to pandas DataFrame
 rodent = pd.DataFrame.from_records(results)
@@ -20,8 +20,8 @@ rodent = rodent.rename(columns={'date_trunc_y_inspection_date': 'inspection_date
 rodent['inspection_date'] = pd.to_datetime(rodent['inspection_date'])
 # print(rodent['inspection_date'])
 rodent['year'] = rodent.inspection_date.dt.to_period("Y")
-sightings = rodent[['zip_code','inspection_date','count_result','year']]
-cuisines = sightings.groupby('zip_code')['count_result'].count()
+sightings = rodent[['zip_code','inspection_date','result','year']]
+cuisines = sightings.groupby('zip_code')['result'].count()
 print(rodent)
 
 
