4509f3b93929b6559ca11469874f678a2ef5eb5c
jlessoff <jlessoff@gmail.com>
2021-12-13 16:16:11 +0100

    Handling missing grades

--- Restaurants.py
+++ Restaurants.py
@@ -1,7 +1,21 @@
 import pandas as pd
 from sodapy import Socrata
+from matplotlib import pyplot as plt
+import numpy as np
 from secret import app_token,username,password
+from datetime import datetime,date
 
+def func(a_):
+    if a_<=14:
+        return 'A_rep'
+    elif 14< a_<= 27:
+        return 'B_rep'
+    elif a_ > 27:
+        return 'C_rep'
+    elif a_==10000:
+        return 'na'
+    else:
+        'unknown'
 
 client = Socrata("data.cityofnewyork.us",
                  app_token=app_token,
@@ -15,5 +29,34 @@ results = client.get("43nn-pn8j", select='date_trunc_ymd(inspection_date),dba,zi
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 results_df = results_df.rename(columns={'date_trunc_ymd_inspection_date': 'inspection_date'}, index={'ONE': 'Row_1'})
-results_df['inspection_date'] = pd.to_datetime(results_df['inspection_date'])
-results_df=results_df.dropna()
+results_df['inspection_date']= pd.to_datetime(results_df['inspection_date'], errors='coerce').dt.date
+results_df['score']=results_df['score'].fillna(10000).astype(int)
+
+
+
+
+results_df['c'] = results_df[['score']].apply(lambda x: func(x[0]), axis=1)
+#print(results_df[['c', 'grade', 'score']])
+#
+# print(results_df['Range'])
+#
+# ## MISSING DATA
+# # print(results_df.isna().sum())
+#
+#
+results_df.isna().sum().reset_index(name="n").plot.bar(x='index', y='n', rot=45)
+plt.show()
+#
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+#
+missing_grade=results_df[results_df['grade'].isnull()]
+print(missing_grade[['score','inspection_type','c']])
+
+
+# cuisines = missing_grade.groupby('score').camis.count().reset_index().sort_values(by = 'score', ascending=False)
+# print(cuisines)
+#
+# print(missing_grade['score'].unique())
+not_missing=results_df[results_df['grade'].notnull()]
+print(not_missing['inspection_type'].unique())
