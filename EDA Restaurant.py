d7858d58db963753fc17f969d7a38e15bb8c1e39
jlessoff <jlessoff@gmail.com>
2022-07-04 14:11:28 +0200

    add map

--- EDA Restaurant.py
+++ EDA Restaurant.py
@@ -18,90 +18,83 @@ plt.show()
 ###CUISINE
 pd.set_option('display.max_columns', None)
 #
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Middle Eastern', 'Moroccan', 'Tapas', 'Spanish', 'Greek',
-#                 'Armenian', 'Basque', 'Afghan', 'Iranian',
-#                 'Turkish'],
-#                 value='Mediterranean')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=[ 'Scandinavian', 'Czech', 'English', 'German',
-#                 'Portuguese', 'Russian', 'Polish', 'Continental', 'Irish',
-#                 'French','Eastern European'], value='European')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Pizza/Italian'], value='Pizza')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Chinese', 'Japanese','Chinese/Japanese', 'Filipino', 'Indian',
-#                 'Indonesian', 'Bangladeshi','Thai', 'Korean', 'Chinese/Cuban',
-#                 'Vietnamese/Cambodian/Malaysia'], value='Asian')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Bottled beverages, including water, sodas, juices, etc.',
-#                 'Café/Coffee/Tea'], value='Beverages')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Bagels/Pretzels', 'Donuts'], value='Baked Goods')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Chilean','Peruvian', 'Caribbean', 'Brazilian',
-#                 'Latin (Cuban, Dominican, Puerto Rican, South & Central American)'],
-#                 value='Latin/Caribbean')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Ice Cream, Gelato, Yogurt, Ices','Seafood', 'Chicken',
-#                 'Nuts/Confectionary', 'Hotdogs/Pretzels', 'Hotdogs',
-#                 'Fruits/Vegetables', 'Vegetarian', 'Salads',
-#                 'Steak', 'Juice, Smoothies, Fruit Salads',
-#                 'Pancakes/Waffles','Vegan',  'Jewish/Kosher'], value='Specialty')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Sandwiches', 'Soups', 'Soup/Sandwiches'],
-#                 value='Soups & Sandwiches')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Delicatessen', 'Sandwiches/Salads/Mixed Buffet'],
-#                 value='Deli/Buffett')
-#
-# results_df['cuisine_description'] = results_df['cuisine_description'].replace(
-#     to_replace=['Soul Food', 'Californian', 'Creole/Cajun', 'Cajun',
-#                 'Creole', 'Barbecue', 'Tex-Mex', 'Southwestern', 'Hamburgers','Hawaiian'],
-#                 value='American')
+
 # #
 
-#get distribution of grade by cuisine type
-# print(len(results_df))
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Middle Eastern', 'Moroccan', 'Tapas', 'Spanish', 'Greek',
+                'Armenian', 'Basque', 'Afghan', 'Iranian',
+                'Turkish'],
+                value='Mediterranean')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=[ 'Scandinavian', 'Czech', 'English', 'German',
+                'Portuguese', 'Russian', 'Polish', 'Continental', 'Irish',
+                'French','Eastern European'], value='European')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Pizza/Italian'], value='Pizza')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Chinese', 'Japanese','Chinese/Japanese', 'Filipino', 'Indian',
+                'Indonesian', 'Bangladeshi','Thai', 'Korean', 'Chinese/Cuban',
+                'Vietnamese/Cambodian/Malaysia'], value='Asian')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Bottled beverages, including water, sodas, juices, etc.',
+                'Café/Coffee/Tea'], value='Beverages')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Bagels/Pretzels', 'Donuts'], value='Baked Goods')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Chilean','Peruvian', 'Caribbean', 'Brazilian',
+                'Latin (Cuban, Dominican, Puerto Rican, South & Central American)'],
+                value='Latin/Caribbean')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Ice Cream, Gelato, Yogurt, Ices','Seafood', 'Chicken',
+                'Nuts/Confectionary', 'Hotdogs/Pretzels', 'Hotdogs',
+                'Fruits/Vegetables', 'Vegetarian', 'Salads',
+                'Steak', 'Juice, Smoothies, Fruit Salads',
+                'Pancakes/Waffles','Vegan',  'Jewish/Kosher'], value='Specialty')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Sandwiches', 'Soups', 'Soup/Sandwiches'],
+                value='Soups & Sandwiches')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Delicatessen', 'Sandwiches/Salads/Mixed Buffet'],
+                value='Deli/Buffett')
+
+results_df['cuisine_description'] = results_df['cuisine_description'].replace(
+    to_replace=['Soul Food', 'Californian', 'Creole/Cajun', 'Cajun',
+                'Creole', 'Barbecue', 'Tex-Mex', 'Southwestern', 'Hamburgers','Hawaiian'],
+                value='American')
+
+
+#get distribution of grade PROXY by cuisine type
 cuisines = results_df.groupby('cuisine_description').camis.count().reset_index().sort_values(by = 'camis', ascending=False)
 cuisines_desc = list(cuisines.head(30)['cuisine_description'])
 cuisinegrade = results_df[['cuisine_description', 'c','score']]
-
 cuisinegrade = cuisinegrade[cuisinegrade['cuisine_description'].isin(cuisines_desc)]
-# print(len(cuisinegrade))
 cuisinegrade = cuisinegrade[cuisinegrade.c.isin(['A_rep', 'B_rep', 'C_rep'])]
 cuisinegrade = cuisinegrade.reset_index().drop('index', axis=1)
 pd.set_option('display.max_colwidth', 50)
 cg = pd.crosstab(cuisinegrade['cuisine_description'], cuisinegrade.c).sort_values(by='A_rep', ascending=False)
-
-#print(cg)
-# print(len(cg))
 cgdensity = cg.apply(lambda r: r/r.sum(), axis=1)
 cgdensity = cgdensity.sort_values(by='A_rep')
 cglabels = list(cgdensity.index)
 cglabels = [ '\n'.join(wrap(l, 20)) for l in cglabels ]
-
 cgdensity.plot(kind='barh', stacked='True', mark_right=True, figsize = (10,8), color=['b','g','r'])
 plt.ylabel('')
 plt.xlim(0, 1.15)
 plt.xlabel('Percentage of restaurants')
 plt.title('Grade dist')
-plt.show()
+plt.savefig('gradebycuisine.png')
 
-##################################################################################
-#
+##get score distribution for individual cuisine type
 cuisinegrade['score']=cuisinegrade['score'].astype(int)
-
-
 mean=[]
 median=[]
 twenty=cuisinegrade['cuisine_description'].value_counts().index
@@ -116,7 +109,16 @@ for i in range(0,20):
     plt.title(f'Score distribution for {twenty[i][:15]} restaurants')
     plt.axvline(x=median, color='r', label=f'Median = {median}', )
     plt.axvline(x=mean, color='g', label=f'Median = {mean}', )
-    plt.show()
+    plt.savefig(i+'scoredistribution.png')
 df = pd.DataFrame(rows, columns=["Name", "Mean", "Median"])
-print(df)
+
+##density
+ig, ax = plt.subplots()
+sns.kdeplot(results_df["score"], shade=True, ax=ax)
+ax.legend()
+fig.suptitle("Health Inspection Violations Density")
+plt.savefig('healthinspectiondensity.png')
+
+
+
 
--- Restaurants.py
+++ Restaurants.py
@@ -176,34 +176,34 @@ df = pd.DataFrame(rows, columns=["Name", "Mean", "Median"])
 
 
 
-#CREATE NYC RESTAURANT MAP
-# results_df=results_df.dropna()
-# nyc = [40.742, -73.956]
-# lat = results_df.latitude.tolist()
-# lng = results_df.longitude.tolist()
+# CREATE NYC RESTAURANT MAP
+results_df=results_df.dropna()
+nyc = [40.742, -73.956]
+lat = results_df.latitude.tolist()
+lng = results_df.longitude.tolist()
 
-#COLOR BASED ON GRADE
-# color_dict = {'A_rep': 'grey', 'B_rep': 'yellow', 'C_rep': 'red'}
-# map_NM = folium.Map(location=nyc,
-#                     zoom_start=11,
-#                     tiles='openstreetmap',
-#                     control_scale=True)
-#
-# for lat, lng, grade in zip(results_df['latitude'],
-#                             results_df['longitude'],
-#                             results_df['c'], ):
-#     label = '{}'.format(grade)
-#     label = folium.Popup(label, parse_html=True)
-#     folium.CircleMarker([lat, lng],
-#                         radius=1,
-#                         popup=label,
-#                         color=[color_dict.get(grade)],
-#                         fill=True,
-#                         fill_color='#3186cc',
-#                         fill_opacity=0.7,
-#                         parse_html=False).add_to(map_NM)
+# COLOR BASED ON GRADE
+color_dict = {'A_rep': 'grey', 'B_rep': 'yellow', 'C_rep': 'red'}
+map_NM = folium.Map(location=nyc,
+                    zoom_start=11,
+                    tiles='openstreetmap',
+                    control_scale=True)
 
-# map_NM.save('restaurantmap.html')
+for lat, lng, grade in zip(results_df['latitude'],
+                            results_df['longitude'],
+                            results_df['c'], ):
+    label = '{}'.format(grade)
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker([lat, lng],
+                        radius=1,
+                        popup=label,
+                        color=[color_dict.get(grade)],
+                        fill=True,
+                        fill_color='#3186cc',
+                        fill_opacity=0.7,
+                        parse_html=False).add_to(map_NM)
+
+map_NM.save('restaurantmap.html')
 
 #heat map of a restaurant
 results_df1=results_df[['latitude','longitude','c']]
