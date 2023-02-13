5b6950f021420af7d8436cf6eecd51916ed437ac
Jackie <61322410+jlessoff@users.noreply.github.com>
2023-02-13 15:42:12 +0100

    Update CausalAnalysis.py

--- CausalAnalysis.py
+++ CausalAnalysis.py
@@ -4,53 +4,76 @@ import pandas as pd
 from tabulate import tabulate
 from Rodents import resultcount
 from Restaurants import results_df
-pd.set_option('display.max_columns', None)
 
+def get_exam_count(results_df):
+    examcount = results_df.groupby('camis').inspection_date.nunique().reset_index().sort_values(by='inspection_date', ascending=False)
+    examcount = examcount.rename(columns={'inspection_date': 'exam_count'})
+    return examcount
 
-##begin creating data frame for the independence test
-###GET EXAM COUNT VARIABLE
-examcount = results_df.groupby('camis').inspection_date.nunique().reset_index().sort_values(by = 'inspection_date', ascending=False)
-examcount = examcount.rename(columns={'inspection_date': 'exam_count'}, index={'ONE': 'Row_1'})
-#AVERAGE VIOLATIONS PER RESTAURANT
-vcount=results_df.groupby(['camis','inspection_date']).violation_code.count().reset_index()
-vcount=round(vcount.groupby(['camis']).violation_code.mean().reset_index())
-vcount['vcount']=vcount['violation_code']
-vcount=vcount.drop(columns=['violation_code',])
-#PIVOT TO GET INDICATOR VARIABLES FOR VIOLATIONS
-violation_df=results_df.loc[results_df.groupby(['violation_code','camis'])['inspection_date'].idxmax()]
-violation_df = violation_df.groupby(['violation_code','camis']).count()
-violation_df=violation_df.pivot_table(index='camis',columns='violation_code',values='violation_description').fillna(0)
-#GET SOME INFORMATION ABOUT RESTAURANTS
-info=results_df.loc[results_df.groupby(['camis'])['inspection_date'].idxmax()]
-info=info[['camis','cuisine_description','neighborhood','zipcode']]
-#MERGE DATA SETS
-df = pd.merge(examcount, vcount, how="left", on='camis')
-df['vcount']=df['vcount'].fillna(0)
-df = pd.merge(df, violation_df, how="left", on='camis')
-df = pd.merge(df, info, how="left", on='camis')
-#READ IN MAPPING FILE FOR ZIP CODE, ETC
-zippop=pd.read_csv('zipcode_pop.csv')
-zipsize=pd.read_csv('zip_size.csv')
-neighborhood=pd.read_csv('nyc-zip-codes.csv')
-zip_data = pd.merge(zipsize, zippop, how="left", on='zip_code')
-zip_data = pd.merge(zip_data, neighborhood, how="left", on='zip_code')
-zip_data = zip_data.groupby('neighborhood').sum().reset_index()
-zip_data = pd.merge(zip_data, resultcount, how="left", on='neighborhood')
-#CREATE VARIABLES FOR POPULATION AND RAT DENSITY
-zip_data['sq_meters'] = pd.to_numeric(zip_data['sq_km'])
-zip_data['Pop'] = pd.to_numeric(zip_data['Pop'])
-zip_data['pop_dens']=(zip_data['Pop'])/(zip_data['sq_km'])
-zip_data['rat_dens']=(zip_data['result'])/(zip_data['sq_km'])
-zip_data=zip_data[['rat_dens','pop_dens','neighborhood']]
-df = pd.merge(df, zip_data, how="left", on='neighborhood')
-#CREATE BUCKETS FOR POPULATION AND RAT DENSITY
-df['pop_dens'] = pd.qcut(df['pop_dens'], q=4)
-df['rat_dens'] = pd.qcut(df['rat_dens'], q=2)
-df.to_csv('df.csv')
-df=pd.read_csv('df.csv')
-df=df.drop(columns=['camis','zipcode'])
+def get_average_violations(results_df):
+    vcount = results_df.groupby(['camis', 'inspection_date']).violation_code.count().reset_index()
+    vcount = round(vcount.groupby(['camis']).violation_code.mean().reset_index())
+    vcount['vcount'] = vcount['violation_code']
+    vcount = vcount.drop(columns=['violation_code'])
+    return vcount
+
+def get_violation_indicator(results_df):
+    violation_df = results_df.loc[results_df.groupby(['violation_code', 'camis'])['inspection_date'].idxmax()]
+    violation_df = violation_df.groupby(['violation_code', 'camis']).count()
+    violation_df = violation_df.pivot_table(index='camis', columns='violation_code', values='violation_description').fillna(0)
+    return violation_df
+
+def get_restaurant_info(results_df):
+    info = results_df.loc[results_df.groupby(['camis'])['inspection_date'].idxmax()]
+    info = info[['camis', 'cuisine_description', 'neighborhood', 'zipcode']]
+    return info
+
+def merge_data_sets(examcount, vcount, violation_df, info):
+    df = pd.merge(examcount, vcount, how="left", on='camis')
+    df['vcount'] = df['vcount'].fillna(0)
+    df = pd.merge(df, violation_df, how="left", on='camis')
+    df = pd.merge(df, info, how="left", on='camis')
+    return df
+
+def get_zip_data():
+    zippop = pd.read_csv('zipcode_pop.csv')
+    zipsize = pd.read_csv('zip_size.csv')
+    neighborhood = pd.read_csv('nyc-zip-codes.csv')
+    zip_data = pd.merge(zipsize, zippop, how="left", on='zip_code')
+    zip_data = pd.merge(zip_data, neighborhood, how="left", on='zip_code')
+    zip_data = zip_data.groupby('neighborhood').sum().reset_index()
+    zip_data = pd.merge(zip_data, resultcount, how="left", on='neighborhood')
+    zip_data['sq_meters'] = pd.to_numeric(zip_data['sq_km'])
+    zip_data['Pop'] = pd.to_numeric(zip_data['Pop'])
+    zip_data['pop_dens'] = (zip_data['Pop'])/(zip_data['sq_km'])
+    zip_data['rat_dens'] = (zip_data['result'])/(zip_data['sq_km'])
+    zip_data = zip_data[['rat_dens', 'pop_dens', 'neighborhood']]
+    return zip_data
+
+def merge_zip_data(df, zip_data):
+    df = pd.merge(df, zip_data, how="left", on='neighborhood')
+    df['pop_dens'] = pd.qcut(df['pop_dens'], q=4)
+    df['rat_dens'] = pd.qcut(df['rat_dens'], q=2)
+    df = df.drop(columns=['camis', 'zipcode'])
+    df.to_csv('df.csv')
+    df = pd.read_csv('df.csv')
+    return df
+
+results_df = pd.read_csv('results.csv')
+
+examcount = get_exam_count(results_df)
+vcount = get_average_violations(results_df)
+violation_df = get_violation_indicator(results_df)
+info = get_restaurant_info(results_df)
+
+df = merge_data_sets(examcount, vcount, violation_df, info)
+
+zip_data = get_zip_data()
+df = merge_zip_data(df, zip_data)
+
 print(df)
 df.reset_index()
+
 
 #create df to just look at relationship between violations
 df1=df[['2.0','3.0','4.0','5.0','6.0','7.0','8.0','9.0','10.0','19.0','22.0']]
